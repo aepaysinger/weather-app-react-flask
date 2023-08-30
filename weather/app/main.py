f84b8c116671d8dfea5a65001b75008e89efbe34
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-08-30 11:47:46 -0500

    setting up weather react app

---
+++ weather/app/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, escape, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f"Hello, {escape(name)}!"
+
+
+def main():
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
